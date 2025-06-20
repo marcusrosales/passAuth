6e2ba12e67da94f8c9e20b98dd5bc58765cde1a2
marcusrosales <marcusrosalesofficial5@gmail.com>
2025-06-20 02:45:13 -0500

    added strucured comments

--- passCheck.py
+++ passCheck.py
@@ -1,6 +1,14 @@
 # Password Requirments #
 
-# 
+# Atleast 8 Characters Total Long
+
+# 1 Or More Upper Case Letters
+# 1 Or More Lower Case Letters
+
+# 1 Or More Number
+# 1 Or More Special Character 
+
+
 
 import sys
 
