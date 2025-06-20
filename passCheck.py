4d393c3d7be699915ea5466fcf9d4cd4a253af50
marcusrosales <marcusrosalesofficial5@gmail.com>
2025-06-20 02:56:11 -0500

    added basic functions and buffer function i thought of on the spot

--- passCheck.py
+++ passCheck.py
@@ -1,6 +1,6 @@
 # Password Requirments #
 
-# Atleast 8 Characters Total Long
+# Atleast 8 Characters Total Long 
 
 # 1 Or More Upper Case Letters
 # 1 Or More Lower Case Letters
@@ -8,4 +8,25 @@
 # 1 Or More Number
 # 1 Or More Special Character 
 
+
 import sys
+import time
+
+def main():
+    userPass = sys.argv[1]
+    authenticate(userPass)
+
+def authenticate(password):
+    if len(password) < 8:
+        print("length short test")
+
+
+
+
+def buffer(BuffTime,Message):
+    time.sleep(BuffTime)
+    print(Message)
+
+
+
+main()
