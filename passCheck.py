7716bee1133e7e1ed7d835e8d517f3f62225d904
marcusrosales <marcusrosalesofficial5@gmail.com>
2025-06-20 03:04:35 -0500

    added starting score out of 10 to see the rating of password!

--- passCheck.py
+++ passCheck.py
@@ -12,21 +12,35 @@
 import sys
 import time
 
+passStartingScore = 10
+
+
 def main():
     userPass = sys.argv[1]
-    authenticate(userPass)
 
-def authenticate(password):
+    buffer('initalzing password',3)
+
+    authenticatePassLength(userPass)
+
+
+
+
+
+def authenticatePassLength(password):
     if len(password) < 8:
         print("length short test")
 
 
 
 
-def buffer(BuffTime,Message):
-    time.sleep(BuffTime)
-    print(Message)
 
+# Takes in any string message, then stalls the process making by any paramater entered time, to make it feel more cryptic? 
+def buffer(Message,BuffTime,):
 
+    print(f" {Message} ")
+
+    for x in range(BuffTime):
+        time.sleep(1)
+        print('...')
 
 main()
